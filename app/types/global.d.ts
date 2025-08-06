dc7d6ee715a7f109654304629732cee7be31fa49
xbkv <one.two.o.two@icloud.com>
2025-08-06 21:14:17 +0900

    型

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -1,26 +1,10 @@
 import { MongoClient } from 'mongodb';
+import DOMPurify from 'dompurify';
 
 declare global {
-  let DOMPurify: any;
-}
-
-export interface Thread {
-  _id: string;
-  title: string;
-  description: string;
-  threadId: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface Post {
-  _id: string;
-  content: string;
-  threadId: string;
-  postNumber: number;
-  author?: string;
-  createdAt: string;
-  updatedAt: string;
+  var io: any;
+  var MongoClient: MongoClient;
+  var DOMPurify: DOMPurify;
 }
 
 export {};
