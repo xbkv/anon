72efc1aba45545864df90afbace8bfef642dca86
xbkv <one.two.o.two@icloud.com>
2025-08-06 21:16:00 +0900

    var to let

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -2,9 +2,9 @@ import { MongoClient } from 'mongodb';
 import DOMPurify from 'dompurify';
 
 declare global {
-  var io: any;
-  var MongoClient: MongoClient;
-  var DOMPurify: DOMPurify;
+  let io: any;
+  let MongoClient: MongoClient;
+  let DOMPurify: DOMPurify;
 }
 
 export {};
